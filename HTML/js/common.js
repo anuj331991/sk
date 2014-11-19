752acab7105964c014f7e5ae41d3be6fbf218b5b
Anuj <anujk@clavax.us>
2014-11-19 16:11:24 +0530

    -first commit

---
+++ HTML/js/common.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+    //for scrollable table used in features page
+    $('#scrollbox2').enscroll({
+        horizontalScrolling: true,
+        verticalTrackClass: 'vertical-track2',
+        verticalHandleClass: 'vertical-handle2',
+        horizontalTrackClass: 'horizontal-track2',
+        horizontalHandleClass: 'horizontal-handle2',
+        cornerClass: 'corner2'
+    });
+});
